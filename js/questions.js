e5a43eb69cc210852d232f415264b8601aa3cdbc
danilo <danilo.nikolic@clockify.me>
2024-02-25 00:14:57 +0100

    dsds

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Koji je glavni grad Spanije?",
+    answer: "Madrid",
+    options: [
+      "Barselona",
+      "Madrid",
+      "Bilbao",
+      "Sevilja"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Koji je glavni grad Francuske?",
+    answer: "Pariz",
+    options: [
+      "Pariz",
+      "Lion",
+      "Monpelje",
+      "Bordo"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Koji je glavni grad Italije?",
+    answer: "Rim",
+    options: [
+      "Napulj",
+      "Torino",
+      "Milano",
+      "Rim"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Koji je glavni grad Nemacke?",
+    answer: "Berlin",
+    options: [
+      "Berlin",
+      "Minhen",
+      "Dortmund",
+      "Dizeldorf"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Koji je glavni grad Holandije?",
+    answer: "Hag",
+    options: [
+      "Hag",
+      "Amsterdam",
+      "Eindhoven",
+      "Alkmar"
+    ]
+  },
+];
